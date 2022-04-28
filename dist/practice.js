3cf61ce9f474171b49c0fd9081d9b7c6aafdcd0e
minsuKang <minsu910725@gmail.com>
2022-04-28 10:19:38 +0900

    typescriptpractice1

--- dist/practice.js
+++ dist/practice.js
@@ -1,3 +1,26 @@
 "use strict";
-const message = 'hello world';
-console.log(message);
+class Queue {
+    constructor() {
+        this.list = [];
+    }
+    get length() {
+        return this.list.length;
+    }
+    enqueue(item) {
+        this.list.push(item);
+    }
+    dequeue() {
+        return this.list.shift();
+    }
+}
+const queue = new Queue();
+queue.enqueue(0);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
